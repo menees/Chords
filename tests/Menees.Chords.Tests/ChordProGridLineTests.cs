5840f6cb9f0d9b17c3e2b2b827c50aed85c086c9
Bill Menees <Bill@Menees.com>
2023-07-23 15:40:18 -0500

    Working on it

--- tests/Menees.Chords.Tests/ChordProGridLineTests.cs
+++ tests/Menees.Chords.Tests/ChordProGridLineTests.cs
@@ -1,11 +1,32 @@
 ﻿namespace Menees.Chords;
 
+using Menees.Chords.Parsers;
+using Shouldly;
+
 [TestClass]
 public class ChordProGridLineTests
 {
 	[TestMethod]
-	public void TryParse()
+	public void TryParseFail()
 	{
-		Assert.Fail();
+		const string Text = "|| A . . . | E . . . |";
+		LineContext context = LineContextTests.Create(Text);
+
+		// ChordPro grid line syntax is ambiguous enough that we don't try to parse.
+		// We just detect if we're inside a start_of_grid/end_of_grid environment.
+		ChordProGridLine? line = ChordProGridLine.TryParse(context);
+		line.ShouldBeNull();
+	}
+
+	[TestMethod]
+	public void TryParseSuccess()
+	{
+		const string Text = "| A . | D . | E . |";
+		Document doc = Document.Parse("{sog}\n" + Text + "\n{eog}\n" + Text);
+		doc.Entries.Count.ShouldBe(4);
+		doc.Entries[0].ShouldBeOfType<ChordProDirectiveLine>().LongName.ShouldBe("start_of_grid");
+		doc.Entries[1].ShouldBeOfType<ChordProGridLine>().Text.ShouldBe(Text);
+		doc.Entries[2].ShouldBeOfType<ChordProDirectiveLine>().LongName.ShouldBe("end_of_grid");
+		doc.Entries[3].ShouldBeOfType<LyricLine>().Text.ShouldBe(Text);
 	}
 }
--- tests/Menees.Chords.Tests/ChordProRemarkLineTests.cs
+++ tests/Menees.Chords.Tests/ChordProRemarkLineTests.cs
@@ -1,11 +1,30 @@
 ﻿namespace Menees.Chords;
 
+using Menees.Chords.Parsers;
+
 [TestClass]
 public class ChordProRemarkLineTests
 {
 	[TestMethod]
 	public void TryParse()
 	{
-		Assert.Fail();
+		Test("Not a remark", false);
+		Test("# Remark", true);
+		Test("  # Remark  ", true);
+
+		static void Test(string text, bool expectLine)
+		{
+			LineContext context = LineContextTests.Create(text);
+			ChordProRemarkLine? line = ChordProRemarkLine.TryParse(context);
+			if (!expectLine)
+			{
+				line.ShouldBeNull();
+			}
+			else
+			{
+				line.ShouldNotBeNull();
+				line.Text.ShouldBe(text);
+			}
+		}
 	}
 }
