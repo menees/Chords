ef72c63dc595be2d7fa6f0a903770f4edd44eb16
Bill Menees <Bill@Menees.com>
2023-07-23 15:09:38 -0500

    Working on it

--- tests/Menees.Chords.Tests/ChordProDirectiveLineTests.cs
+++ tests/Menees.Chords.Tests/ChordProDirectiveLineTests.cs
@@ -1,17 +1,64 @@
 ﻿namespace Menees.Chords;
 
+using Menees.Chords.Parsers;
+
 [TestClass]
 public class ChordProDirectiveLineTests
 {
 	[TestMethod]
 	public void TryParse()
 	{
-		Assert.Fail();
+		Test("{name}", "name");
+		Test("{name: argument}", "name", "argument");
+		Test("{name:argument}", "name", "argument");
+		Test(" { name : argument } ", "name", "argument");
+
+		Test("{start_of_tab: Solo}", "start_of_tab", "Solo", "sot");
+		Test("{eot}", "eot", null, "end_of_tab");
+
+		static void Test(string text, string? expectedName = null, string? expectedArgument = null, string? expectedAlternateName = null)
+		{
+			LineContext context = LineContextTests.Create(text);
+			ChordProDirectiveLine? line = ChordProDirectiveLine.TryParse(context);
+
+			if (expectedName is null)
+			{
+				line.ShouldBeNull();
+			}
+			else
+			{
+				line.ShouldNotBeNull();
+				line.Name.ShouldBe(expectedName);
+				line.Argument.ShouldBe(expectedArgument);
+
+				if (expectedAlternateName is not null)
+				{
+					if (line.Name == line.LongName)
+					{
+						line.ShortName.ShouldBe(expectedAlternateName);
+					}
+					else
+					{
+						line.LongName.ShouldBe(expectedAlternateName);
+					}
+				}
+			}
+		}
 	}
 
 	[TestMethod]
 	public void ToStringTest()
 	{
-		Assert.Fail();
+		Test("{name}");
+		Test("{name: argument}");
+		Test("{name:argument}", "{name: argument}");
+		Test(" { name : argument } ", "{name: argument}");
+
+		static void Test(string text, string? expectedText = null)
+		{
+			LineContext context = LineContextTests.Create(text);
+			ChordProDirectiveLine line = ChordProDirectiveLine.TryParse(context).ShouldNotBeNull();
+			line.ToString().ShouldBe(expectedText ?? text);
+		}
 	}
 }
