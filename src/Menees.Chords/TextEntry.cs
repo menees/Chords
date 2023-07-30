104a1c24e3d870cbcbb2faf92f5fc7c88165a5ad
Bill Menees <Bill@Menees.com>
2023-07-29 19:15:35 -0500

    Working on it

--- src/Menees.Chords/TextEntry.cs
+++ src/Menees.Chords/TextEntry.cs
@@ -5,6 +5,12 @@
 /// </summary>
 public abstract class TextEntry : Entry
 {
+	#region Private Data Members
+
+	private List<Entry>? annotations;
+
+	#endregion
+
 	#region Constructors
 
 	/// <summary>
@@ -25,6 +31,15 @@ public abstract class TextEntry : Entry
 	/// </summary>
 	public string Text { get; }
 
+	/// <summary>
+	/// Gets the ordered sub-entries contained on the same line with this entry.
+	/// </summary>
+	/// <remarks>
+	/// This is useful for entries like a <see cref="HeaderLine"/> that also contains a <see cref="Comment"/>,
+	/// and for when <see cref="ChordDefinitions"/> are at the end of <see cref="ChordLine"/>.
+	/// </remarks>
+	public IReadOnlyList<Entry> Annotations => this.annotations ?? (IReadOnlyList<Entry>)Array.Empty<Entry>();
+
 	#endregion
 
 	#region Public Methods
@@ -33,6 +48,20 @@ public abstract class TextEntry : Entry
 	/// Returns <see cref="Text"/>.
 	/// </summary>
 	public override string ToString() => this.Text;
+
+	#endregion
+
+	#region Protected Methods
+
+	/// <summary>
+	/// Adds a sub-entry that's on the same line as the current entry.
+	/// </summary>
+	/// <param name="annotation">The sub-entry to add.</param>
+	protected void AddAnnotation(Entry annotation)
+	{
+		this.annotations ??= new();
+		this.annotations.Add(annotation);
+	}
 
 	#endregion
 }
