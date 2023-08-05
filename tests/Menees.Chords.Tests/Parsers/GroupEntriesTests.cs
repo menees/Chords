d36210ef985014e5c475d215b2d72c93216977e8
Bill Menees <Bill@Menees.com>
2023-08-05 12:24:23 -0500

    Working on tests

--- tests/Menees.Chords.Tests/Parsers/GroupEntriesTests.cs
+++ tests/Menees.Chords.Tests/Parsers/GroupEntriesTests.cs
@@ -3,10 +3,22 @@
 [TestClass]
 public class GroupEntriesTests
 {
+	#region Public Methods
+
 	[TestMethod]
 	public void ByChordLinePair()
 	{
-		Assert.Fail();
+		IReadOnlyList<Entry> entries = GetEntries(
+			"""
+			A       E    D       E
+			 Oh yeah life goes on
+			A               E         D             A
+			 Long after the thrill of livin' is gone
+			""",
+			GroupEntries.ByChordLinePair);
+		entries.Count.ShouldBe(2);
+		entries[0].ShouldBeOfType<ChordLyricPair>();
+		entries[1].ShouldBeOfType<ChordLyricPair>();
 	}
 
 	[TestMethod]
@@ -18,12 +30,56 @@ public class GroupEntriesTests
 	[TestMethod]
 	public void ByHeaderLine()
 	{
-		Assert.Fail();
+		IReadOnlyList<Entry> entries = GetEntries(
+			"""
+			[Verse 2]
+			A                   E      D                E
+			 Suckin' on a chili dog outside Tastee Freez
+
+			[Chorus]
+			A        E D           E
+			 Oh yeah,  life goes on
+			""",
+			GroupEntries.ByHeaderLine);
+		entries.Count.ShouldBe(2);
+		Section section = entries[0].ShouldBeOfType<Section>();
+		section.Entries.Count.ShouldBe(4);
+		section.Entries[0].ShouldBeOfType<HeaderLine>();
+
+		section = entries[1].ShouldBeOfType<Section>();
+		section.Entries.Count.ShouldBe(3);
+		section.Entries[0].ShouldBeOfType<HeaderLine>();
 	}
 
 	[TestMethod]
 	public void ByBlankLine()
 	{
-		Assert.Fail();
+		IReadOnlyList<Entry> entries = GetEntries(
+			"""
+			A       E    D       E
+			 Oh yeah life goes on
+
+			A               E         D             A
+			 Long after the thrill of livin' is gone
+			""",
+			GroupEntries.ByBlankLine);
+		entries.Count.ShouldBe(3);
+		entries[0].ShouldBeOfType<Section>().Entries.Count.ShouldBe(2);
+		entries[1].ShouldBeOfType<BlankLine>();
+		entries[2].ShouldBeOfType<Section>().Entries.Count.ShouldBe(2);
 	}
+
+	#endregion
+
+	#region Private Methods
+
+	private static IReadOnlyList<Entry> GetEntries(string text, Func<GroupContext, IReadOnlyList<Entry>> grouper)
+	{
+		DocumentParser parser = new(groupers: new[] { grouper });
+		Document document = Document.Parse(text, parser);
+		IReadOnlyList<Entry> result = document.Entries;
+		return result;
+	}
+
+	#endregion
 }
