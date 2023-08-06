cb620634dfb97e8008ac7c0af284d0d0ff5496e0
Bill Menees <Bill@Menees.com>
2023-08-06 12:22:26 -0500

    Working on tests

--- tests/Menees.Chords.Tests/Parsers/GroupEntriesTests.cs
+++ tests/Menees.Chords.Tests/Parsers/GroupEntriesTests.cs
@@ -16,6 +16,7 @@ public class GroupEntriesTests
 			 Long after the thrill of livin' is gone
 			""",
 			GroupEntries.ByChordLinePair);
+
 		entries.Count.ShouldBe(2);
 		entries[0].ShouldBeOfType<ChordLyricPair>();
 		entries[1].ShouldBeOfType<ChordLyricPair>();
@@ -24,7 +25,33 @@ public class GroupEntriesTests
 	[TestMethod]
 	public void ByChordProEnvironment()
 	{
-		Assert.Fail();
+		IReadOnlyList<Entry> entries = GetEntries(
+			"""
+			{start_of_verse}
+			{comment: D* = D Dsus4 D Dsus2 D}
+			It's [D*]all the same, [Cadd9]only the names will [G]change
+			[Cadd9]Everyday[G] it seems we're [G]wast[F]ing [D]away
+			[D*]Another place where the [Cadd9]faces are so [G]cold
+			I'd [Cadd9]drive all [G]night just to [G]get [F]back [D]home
+			{end_of_verse}
+
+			{start_of_chorus}
+			I'm a [Cadd9]cowboy[G].  On a [F]steel horse I [D]ride.
+			I'm [Cadd9]wanted[G]  [F]dead or [D]alive
+			[Cadd9]Wanted[G]  [F]dead or [D]alive
+			{end_of_chorus}
+			""",
+			GroupEntries.ByChordProEnvironment);
+
+		entries.Count.ShouldBe(3);
+		Section section = entries[0].ShouldBeOfType<Section>();
+		section.Entries.Count.ShouldBe(7);
+		section.Entries[0].ShouldBeOfType<ChordProDirectiveLine>().ShortName.ShouldBe("sov");
+
+		entries[1].ShouldBeOfType<BlankLine>();
+		section = entries[2].ShouldBeOfType<Section>();
+		section.Entries.Count.ShouldBe(5);
+		section.Entries[0].ShouldBeOfType<ChordProDirectiveLine>().ShortName.ShouldBe("soc");
 	}
 
 	[TestMethod]
@@ -41,6 +68,7 @@ public class GroupEntriesTests
 			 Oh yeah,  life goes on
 			""",
 			GroupEntries.ByHeaderLine);
+
 		entries.Count.ShouldBe(2);
 		Section section = entries[0].ShouldBeOfType<Section>();
 		section.Entries.Count.ShouldBe(4);
@@ -63,6 +91,7 @@ public class GroupEntriesTests
 			 Long after the thrill of livin' is gone
 			""",
 			GroupEntries.ByBlankLine);
+
 		entries.Count.ShouldBe(3);
 		entries[0].ShouldBeOfType<Section>().Entries.Count.ShouldBe(2);
 		entries[1].ShouldBeOfType<BlankLine>();
