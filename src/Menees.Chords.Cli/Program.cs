07b55aefcd232b947da153e6dcd240aef69790f9
Bill Menees <Bill@Menees.com>
2023-08-17 17:46:05 -0500

    Add ideas for future commands

--- src/Menees.Chords.Cli/Program.cs
+++ src/Menees.Chords.Cli/Program.cs
@@ -9,6 +9,11 @@ internal static class Program
 	{
 		RootCommand root = new("Commands for working with chord sheet files.");
 
+		// LONG-TERM-TODO: Other possible commands:
+		// transpose - +N use sharps. -N use flats. Like https://www.chordpro.org/chordpro/using-chordpro/#transpose
+		// normalize - Call Chord.Normalize() for every parsed Chord in a document.
+		// compact - Replace identical start/end_of_chorus sections with {chorus} directive. One blank line per section.
+		// format - sub-command instead of as an option on the convert command. See Formats enum.
 		Command convert = ConvertCommand.Create();
 		root.Add(convert);
 
