204e06a5af5db82e41987b71862635bbe05cd743
Bill Menees <Bill@Menees.com>
2023-08-17 06:51:56 -0500

    Working on CLI

--- src/Menees.Chords.Cli/ConvertCommand.cs
+++ src/Menees.Chords.Cli/ConvertCommand.cs
@@ -18,12 +18,19 @@ internal sealed class ConvertCommand : BaseCommand
 {
 	#region Private Data Members
 
+	private const string ReadStdIn = "-";
+
+	#endregion
+
+	#region Private Data Members
+
 	private readonly Parsers parsers;
 	private readonly Transformers transformers;
 	private readonly Encoding encoding;
-	private readonly FileInfo input;
+	private readonly FileInfo? input;
 	private readonly FileInfo? output;
-	private readonly bool force;
+	private readonly bool overwrite;
+	private readonly Formats format;
 
 	#endregion
 
@@ -31,12 +38,13 @@ internal sealed class ConvertCommand : BaseCommand
 
 	private ConvertCommand(
 		InvocationContext context,
-		FileInfo input,
+		FileInfo? input,
 		Parsers parsers,
 		Transformers transformers,
 		Encoding encoding,
 		FileInfo? output,
-		bool force)
+		bool overwrite,
+		Formats format)
 		: base(context)
 	{
 		this.input = input;
@@ -44,7 +52,8 @@ internal sealed class ConvertCommand : BaseCommand
 		this.transformers = transformers;
 		this.encoding = encoding;
 		this.output = output;
-		this.force = force;
+		this.overwrite = overwrite;
+		this.format = format;
 	}
 
 	#endregion
@@ -55,34 +64,68 @@ internal sealed class ConvertCommand : BaseCommand
 	{
 		Command result = new("convert", "Converts a chord sheet file from one format to another.");
 
-		Argument<FileInfo?> inputArgument = new(nameof(input), description: "The file to convert.");
+		Argument<FileInfo?> inputArgument = new(
+			nameof(input),
+			description: $"The file to convert. Use \"{ReadStdIn}\" to read from stdin.",
+			parse: parseResult =>
+			{
+				// https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial#add-subcommands-and-custom-validation
+				FileInfo? fileInfo = null;
+				string? filePath = parseResult.Tokens.Single().Value;
+				if (filePath == ReadStdIn)
+				{
+					if (!Console.IsInputRedirected)
+					{
+						parseResult.ErrorMessage = $"Input \"{ReadStdIn}\" is only supported when stdin is redirected (i.e., piped).";
+					}
+				}
+				else
+				{
+					if (File.Exists(filePath))
+					{
+						fileInfo = new FileInfo(filePath);
+					}
+					else
+					{
+						parseResult.ErrorMessage = "Input file does not exist.";
+					}
+				}
+
+				return fileInfo;
+			});
 		result.Add(inputArgument);
 
 		Option<FileInfo> outputOption = new(new[] { "--output", "-o" }, "The output file name. Omit to write to stdout.");
 		result.Add(outputOption);
 
-		Option<bool> forceOption = new(new[] { "--force", "-f" }, "Whether to overwrite the output file if it already exists.");
-		result.Add(forceOption);
+		Option<bool> overwriteOption = new(new[] { "--overwrite", "-y" }, "Whether to overwrite the output file if it already exists.");
+		result.Add(overwriteOption);
 
-		Option<Parsers> parseOption = new(new[] { "--parse", "-p" }, "How the input file should be parsed.");
+		Option<Parsers> parseOption = new(new[] { "--parse", "-p" }, () => Parsers.Default, "How the input file should be parsed.");
 		result.Add(parseOption);
 
-		Option<Transformers> transformOption = new(new[] { "--transform", "-t" }, "How the input should be transformed.");
+		Option<Transformers> transformOption = new(
+			new[] { "--transform", "-t" }, () => Transformers.ChordPro, "How the input should be transformed in memory.");
 		result.Add(transformOption);
 
-		Option<string?> encodingOption = new(new[] { "--encoding", "-e" }, "How the input file is encoded.");
+		Option<Formats> formatOption = new(new[] { "--format", "-f" }, () => Formats.Text, "How the output should be formatted.");
+		result.Add(formatOption);
+
+		const string DefaultEncoding = "UTF-8";
+		Option<string?> encodingOption = new(new[] { "--encoding", "-e" }, () => DefaultEncoding, "How the input and output text is encoded.");
 		result.Add(encodingOption);
 
 		result.SetHandler(context =>
 		{
-			FileInfo input = GetArgumentValue(context, inputArgument) ?? throw new ArgumentException("An input file is required.");
+			FileInfo? input = GetArgumentValue(context, inputArgument);
 			Parsers parsers = GetOptionValue(context, parseOption);
 			Transformers transformers = GetOptionValue(context, transformOption);
-			Encoding encoding = Encoding.GetEncoding(GetOptionValue(context, encodingOption) ?? "UTF-8");
+			Encoding encoding = Encoding.GetEncoding(GetOptionValue(context, encodingOption) ?? DefaultEncoding);
 			FileInfo? output = GetOptionValue(context, outputOption);
-			bool force = GetOptionValue(context, forceOption);
+			bool overwrite = GetOptionValue(context, overwriteOption);
+			Formats format = GetOptionValue(context, formatOption);
 
-			ConvertCommand command = new(context, input, parsers, transformers, encoding, output, force);
+			ConvertCommand command = new(context, input, parsers, transformers, encoding, output, overwrite, format);
 			return command.ExecuteAsync();
 		});
 
@@ -95,38 +138,72 @@ internal sealed class ConvertCommand : BaseCommand
 
 	protected override Task OnExecuteAsync()
 	{
-		if (this.output is not null && this.output.Exists && !this.force)
+		Document inputDocument = this.ParseInput();
+		Document outputDocument = this.TransformInMemory(inputDocument);
+		string outputText = this.FormatOutputText(outputDocument);
+		this.WriteOutput(outputText);
+		return Task.CompletedTask;
+	}
+
+	#endregion
+
+	#region Private Methods
+
+	private Document ParseInput()
+	{
+		bool readStdIn = this.input is null;
+		TextReader reader = readStdIn ? Console.In : new StreamReader(this.input!.FullName, this.encoding, true);
+		try
 		{
-			this.WriteErrorLine("The specified output file already exists and the --force option was not specified.");
-			this.ExitCode = 1;
-		}
-		else
-		{
-			using TextReader reader = new StreamReader(this.input.FullName, this.encoding, true);
 			DocumentParser parser = new(
 				this.parsers == Parsers.ChordPro
 					? DocumentParser.ChordProLineParsers
 					: DocumentParser.DefaultLineParsers);
 			Document inputDocument = Document.Load(reader, parser);
-
-			ChordProTransformer transformer = this.transformers == Transformers.MobileSheets
-				? new MobileSheetsTransformer(inputDocument)
-				: new ChordProTransformer(inputDocument);
-			Document outputDocument = transformer.ToChordPro().Document;
-
-			TextFormatter formatter = new(outputDocument);
-			string outputText = formatter.ToString();
-			if (this.output is null)
+			return inputDocument;
+		}
+		finally
+		{
+			if (!readStdIn)
 			{
-				this.WriteLine(outputText);
-			}
-			else
-			{
-				File.WriteAllText(this.output.FullName, outputText, this.encoding);
+				reader.Dispose();
 			}
 		}
+	}
 
-		return Task.CompletedTask;
+	private Document TransformInMemory(Document inputDocument)
+	{
+		ChordProTransformer transformer = this.transformers == Transformers.MobileSheets
+			? new MobileSheetsTransformer(inputDocument)
+			: new ChordProTransformer(inputDocument);
+		Document outputDocument = transformer.ToChordPro().Document;
+		return outputDocument;
+	}
+
+	private string FormatOutputText(Document outputDocument)
+	{
+		ContainerFormatter formatter = this.format == Formats.Xml
+			? new XmlFormatter(outputDocument)
+			: new TextFormatter(outputDocument);
+		string outputText = formatter.ToString();
+		return outputText;
+	}
+
+	private void WriteOutput(string outputText)
+	{
+		if (this.output is null)
+		{
+			this.Write(outputText);
+		}
+		else if (this.overwrite || !this.output.Exists)
+		{
+			File.WriteAllText(this.output.FullName, outputText, this.encoding);
+		}
+		else
+		{
+			this.WriteErrorLine("The specified output file already exists, and the --overwrite option was not used.");
+			this.ExitCode = 1;
+		}
 	}
 
 	#endregion
---
+++ src/Menees.Chords.Cli/Formats.cs
@@ -0,0 +1,9 @@
+﻿namespace Menees.Chords.Cli;
+
+internal enum Formats
+{
+	Text,
+	Xml,
+
+	// LONG-TERM-TODO: Support HTML, PDF, LaTeX (https://ctan.org/pkg/songs).
+}
