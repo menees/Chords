122826936a2cedf1130a45c5b07688e2439a06fe
Bill Menees <Bill@Menees.com>
2023-09-05 20:59:21 -0500

    Parse ChordProGridLine as segments

--- src/Menees.Chords/ChordProGridLine.cs
+++ src/Menees.Chords/ChordProGridLine.cs
@@ -2,6 +2,7 @@
 
 #region Using Directives
 
+using System.Linq;
 using Menees.Chords.Parsers;
 
 #endregion
@@ -14,12 +15,12 @@ using Menees.Chords.Parsers;
 /// <c>|  Am . . . | C . . . | E  . . . | E  . . . |</c>
 /// </remarks>
 /// <seealso href="https://www.chordpro.org/chordpro/directives-env_grid/"/>
-public sealed class ChordProGridLine : TextEntry
+public sealed class ChordProGridLine : SegmentedEntry
 {
 	#region Constructors
 
-	private ChordProGridLine(string text)
-		: base(text)
+	private ChordProGridLine(IReadOnlyList<TextSegment> segments)
+		: base(segments)
 	{
 	}
 
@@ -40,10 +41,37 @@ public sealed class ChordProGridLine : TextEntry
 
 		// The ChordPro grid line syntax is very loose, and ChordPro's examples include things
 		// that the documentation says are not allowed. So, we'll require the line to be inside
-		// an open start_of_grid/end_of_grid section.
-		if (context.State.TryGetValue(ChordProDirectiveLine.GridStateKey, out object? gridState) && gridState is ChordProDirectiveLine)
+		// an open start_of_grid/end_of_grid section, or the line has to have several '|' separators.
+		// If we're not in an explicit grid, then we have to be careful not to match tab lines.
+		bool inExplicitGrid = context.State.TryGetValue(ChordProDirectiveLine.GridStateKey, out object? gridState) && gridState is ChordProDirectiveLine;
+		const int MinSeparators = 3;
+		if (inExplicitGrid || context.LineText.Count(ch => ch == '|') >= MinSeparators)
 		{
-			result = new ChordProGridLine(context.LineText);
+			IReadOnlyList<TextSegment> segments = TryGetSegments(
+				context,
+				false,
+				token => inExplicitGrid ? new TextSegment(token.Text) : null,
+				out IReadOnlyList<Entry> annotations);
+
+			if (segments.Count > 0)
+			{
+				if (inExplicitGrid)
+				{
+					result = new ChordProGridLine(segments);
+				}
+				else
+				{
+					// Don't allow Nashville-numbered chords because it's ambiguous with TablatureLines.
+					List<ChordSegment> chords = segments.OfType<ChordSegment>().ToList();
+					if (chords.Count >= (MinSeparators - 1)
+						&& chords.All(cs => cs.Chord.Notation == Notation.Name || cs.Chord.Notation == Notation.Roman))
+					{
+						result = new ChordProGridLine(segments);
+					}
+				}
+
+				result?.AddAnnotations(annotations);
+			}
 		}
 
 		return result;
--- tests/Menees.Chords.Tests/ChordProGridLineTests.cs
+++ tests/Menees.Chords.Tests/ChordProGridLineTests.cs
@@ -9,24 +9,36 @@ public class ChordProGridLineTests
 	[TestMethod]
 	public void TryParseInvalidTest()
 	{
-		const string Text = "|| A . . . | E . . . |";
-		LineContext context = LineContextTests.Create(Text);
+		Test("| A . . . |"); // Too short; not enough separators or chords
+		Test("|--1-2-3---|--1-2-3---|"); // Tab line
+		Test("| 1 . . . | 4 . . . | 5 . . . | 1 . . . |"); // Nashville numbered-chords are too much like tab lines.
 
-		// ChordPro grid line syntax is ambiguous enough that we don't try to parse.
-		// We just detect if we're inside a start_of_grid/end_of_grid environment.
-		ChordProGridLine? line = ChordProGridLine.TryParse(context);
-		line.ShouldBeNull();
+		static void Test(string text)
+		{
+			LineContext context = LineContextTests.Create(text);
+			ChordProGridLine? line = ChordProGridLine.TryParse(context);
+			line.ShouldBeNull();
+		}
 	}
 
 	[TestMethod]
 	public void TryParseValidTest()
 	{
-		const string Text = "| A . | D . | E . |";
-		Document doc = Document.Parse("{sog}\n" + Text + "\n{eog}\n" + Text, DocumentParserTests.Ungrouped());
-		doc.Entries.Count.ShouldBe(4);
-		doc.Entries[0].ShouldBeOfType<ChordProDirectiveLine>().LongName.ShouldBe("start_of_grid");
-		doc.Entries[1].ShouldBeOfType<ChordProGridLine>().Text.ShouldBe(Text);
-		doc.Entries[2].ShouldBeOfType<ChordProDirectiveLine>().LongName.ShouldBe("end_of_grid");
-		doc.Entries[3].ShouldBeOfType<ChordLine>().ToString().ShouldBe(Text);
+		Test("| A . | D . | E . |", "A", "D", "E");
+		Test("|| Am . . . | C . . . | D  . . . | F  . . . |", "Am", "C", "D", "F");
+		Test("|  Am . . . | E . . . | Am . . . | Am . . . ||", "Am", "E", "Am", "Am");
+		Test("|: C7 . | %  . :|: G7 . | % . :|   4x", "C7", "G7");
+
+		static void Test(string text, params string[] chords)
+		{
+			Document doc = Document.Parse("{sog}\n" + text + "\n{eog}\n" + text, DocumentParserTests.Ungrouped());
+			doc.Entries.Count.ShouldBe(4);
+			doc.Entries[0].ShouldBeOfType<ChordProDirectiveLine>().LongName.ShouldBe("start_of_grid");
+			doc.Entries[1].ShouldBeOfType<ChordProGridLine>().ToString().ShouldBe(text);
+			doc.Entries[2].ShouldBeOfType<ChordProDirectiveLine>().LongName.ShouldBe("end_of_grid");
+			ChordProGridLine line = doc.Entries[3].ShouldBeOfType<ChordProGridLine>();
+			line.ToString().ShouldBe(text);
+			line.Segments.OfType<ChordSegment>().Select(cs => cs.Chord.Name).ShouldBe(chords);
+		}
 	}
 }
