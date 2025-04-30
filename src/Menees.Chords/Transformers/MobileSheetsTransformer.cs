7b9513aaa7088bba9ac533c559c9c3c9415b4c74
Bill Menees <Bill@Menees.com>
2025-04-29 19:16:43 -0500

    Stop using grid directives for MobileSheets

--- src/Menees.Chords/Transformers/MobileSheetsTransformer.cs
+++ src/Menees.Chords/Transformers/MobileSheetsTransformer.cs
@@ -13,6 +13,12 @@ using Menees.Chords.Parsers;
 /// <see href="https://www.zubersoft.com/mobilesheets/"/>
 public sealed class MobileSheetsTransformer : ChordProTransformer
 {
+	#region Private Data Members
+
+	private const StringComparison Comparison = ChordParser.Comparison;
+
+	#endregion
+
 	#region Constructors
 
 	/// <inheritdoc/>
@@ -45,13 +51,13 @@ public sealed class MobileSheetsTransformer : ChordProTransformer
 		IReadOnlyList<Entry> chordPro = base.TransformEntries(supportedInput);
 		List<Entry> result = new(chordPro.Count);
 
-		const StringComparison Comparison = ChordParser.Comparison;
 		foreach (Entry entry in chordPro)
 		{
 			if (entry is ChordProGridLine grid)
 			{
-				// MobileSheets tries to render chord grid lines with chords on a line above the separators,
+				// MobileSheets pre-v3.9.0 tried to render chord grid lines with chords on a line above the separators,
 				// so we'll convert all the separators to ChordPro annotations to get them on the same (upper) line.
+				// In v3.9.0, MobileSheets chord grid support auto-sizes to the page width, which is still undesirable.
 				List<TextSegment> brackted = [.. grid.Segments
 					.Select(s => s is ChordSegment c ? new ChordSegment(c.Chord, $"[{c.Text}]")
 						: s is WhiteSpaceSegment ws ? s
@@ -65,14 +71,8 @@ public sealed class MobileSheetsTransformer : ChordProTransformer
 			else if (directive.ShortName.Equals("sob", Comparison) || directive.ShortName.Equals("eob", Comparison))
 			{
 				// MobileSheets doesn't support start/end_of_bridge directives as of v3.8.12 (2023-08-15).
-				const string LongSuffix = "bridge";
-				int suffixLength = directive.Name.EndsWith(LongSuffix, Comparison) ? LongSuffix.Length : 1;
-				string newName = directive.Name[0..^suffixLength] + (suffixLength == 1 ? "v" : "verse");
-
 				// MobileSheets won't show a header at all for {sov}, so we'll make it {sov: Bridge}.
-				string? argument = directive.Name.StartsWith("s", Comparison) && string.IsNullOrEmpty(directive.Argument)
-					? "Bridge" : directive.Argument;
-				ChordProDirectiveLine transformed = new(newName, argument);
+				ChordProDirectiveLine transformed = ReplaceWithVerseDirective(directive, "bridge", "Bridge");
 				result.Add(transformed);
 			}
 			else if (directive.ShortName.Equals("sov", Comparison) && string.IsNullOrEmpty(directive.Argument))
@@ -81,12 +81,36 @@ public sealed class MobileSheetsTransformer : ChordProTransformer
 				ChordProDirectiveLine transformed = new(directive.Name, "Verse");
 				result.Add(transformed);
 			}
+			else if (directive.ShortName.Equals("sog", Comparison) || directive.ShortName.Equals("eog", Comparison))
+			{
+				// MobileSheets v3.9.0 added support for chord grids, but they auto-size to the page width,
+				// so it'll only show one column per page. Yuck! https://zubersoft.com/mobilesheets/forum/thread-12271.html
+#pragma warning disable CC0021 // Use nameof. This suffix parameter has nothing to do with the earlier variable.
+				ChordProDirectiveLine transformed = ReplaceWithVerseDirective(directive, "grid");
+#pragma warning restore CC0021 // Use nameof
+				result.Add(transformed);
+			}
 			else
 			{
 				result.Add(entry);
 			}
 		}
 
+		return result;
+	}
+
+	#endregion
+
+	#region Private Methods
+
+	private static ChordProDirectiveLine ReplaceWithVerseDirective(ChordProDirectiveLine directive, string longSuffix, string? defaultArgument = null)
+	{
+		int suffixLength = directive.Name.EndsWith(longSuffix, Comparison) ? longSuffix.Length : 1;
+		string newName = directive.Name[0..^suffixLength] + (suffixLength == 1 ? "v" : "verse");
+
+		string? argument = directive.Name.StartsWith("s", Comparison) && string.IsNullOrEmpty(directive.Argument)
+			? defaultArgument : directive.Argument;
+		ChordProDirectiveLine result = new(newName, argument);
 		return result;
 	}
 
